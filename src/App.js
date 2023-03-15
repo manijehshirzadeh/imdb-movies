f8bbcdd51c21005d6648b39c80e1ea89a13a70a3
Manijeh <manijeh.shirzadeh63@gmail.com>
2023-03-16 00:05:23 +1100

    import Material UI and create list items

--- src/App.js
+++ src/App.js
@@ -1,30 +1,12 @@
+import * as React from 'react';
 import { useState, useEffect } from "react";
 import "./App.css";
-
-// function App() {
-//   const [posts, setPosts] = useState([]);
-// useEffect (() => {
-//   getPosts()
-//   },[])
-
-//   const getPosts = async () =>{
-//     await fetch("https://jsonplaceholder.typicode.com/posts")
-//     .then((response) => response.json())
-//     // .then(res=>console.log({res}))
-//     .then((fetchedposts) => setPosts(fetchedposts))
-//   }
-// console.log({posts})
-//   const listPosts = posts.map(post => <p >{(post)}</p>)
-//   return (
-//     <div>
-//       Hey Manijeh
-//       {posts.length !== 0  && listPosts}
-
-//     </div>
-//   );
-// }
-
-// export default App;
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import Avatar from '@mui/material/Avatar';
+import Typography from '@mui/material/Typography';
 
 
 
@@ -44,41 +26,34 @@ function App() {
     
   console.log({posts})
   return (
-    <div>
+    
+    <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
       {posts.map((item) => (
-        <p key={item.id}>{item.title}</p>
+        <ListItem key={item.id}>
+          <ListItemAvatar>
+          <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+        </ListItemAvatar>
+        <ListItemText
+          secondary={
+            <React.Fragment>
+              <Typography
+                sx={{ display: 'inline' }}
+                component="span"
+                variant="body2"
+                color="text.primary"
+              >  
+              </Typography>
+            
+            </React.Fragment>
+          }
+        />
+          {item.title}</ListItem>
       ))}
-    </div>
+ </List>
   );
 }
   
   export default App
 
 
-
-// function Products() {
-
-//   const [products, setProducts] = useState({});
   
-//   const fetchProducts = async () => {
-//   const {data} = await Axios.get('https://api.test.ts/demo/test');  
-//   const products= data
-//   setProducts(products);
-//   };
-  
-//   useEffect(() => {
-  
-//     fetchProducts()
-  
-//    }, []);
-  
-//    return(
-       
-//        <div>
-//            Want to Display list of products from API
-//        </div>
-//    )
-  
-//   }
-  
-//   export default Products;
